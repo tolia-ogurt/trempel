a06d0da23bc43e0eb4a2b6ad364055ca6ddd7b5f
tolia-ogurt <tolia.ogurt@gmail.com>
2021-11-25 15:59:55 +0200

    fixed bugs last commit

--- app/src/main/java/com/example/trempel/PdpFragment.kt
+++ app/src/main/java/com/example/trempel/PdpFragment.kt
@@ -4,20 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class PdpFragment:Fragment() {
-    lateinit var productDescription: TextView
+class PdpFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.pdp_fragment, container, false)
-        productDescription = rootView.findViewById(R.id.productDescription)
-        return rootView
+        return inflater.inflate(R.layout.pdp_fragment, container, false)
     }
 
 }
