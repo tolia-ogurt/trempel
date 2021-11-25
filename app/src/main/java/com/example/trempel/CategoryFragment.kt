9ee6e5cf4f2c21c82c8ac3a59d00aec2c47ec264
tolia-ogurt <tolia.ogurt@gmail.com>
2021-11-25 13:24:03 +0200

    added navigation

---
+++ app/src/main/java/com/example/trempel/CategoryFragment.kt
@@ -0,0 +1,26 @@
+package com.example.trempel
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.trempel.databinding.CategoryFragmentBinding
+
+class CategoryFragment : Fragment(R.layout.category_fragment) {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = CategoryFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.ivCategory.setOnClickListener {
+            findNavController().navigate(R.id.action_categoryFragment_to_pdpFragment)
+        }
+        return view
+    }
+}
